e8c9739f247fe9c21102d2a30954d128b7feb6e8
Anna_Bondarchuk <anna_bondarchuk@epam.com>
2018-10-01 17:26:40 +0300

    added rss sources

---
+++ parser.php
@@ -0,0 +1,13 @@
+
+<?php
+
+//$content = simplexml_load_file('http://blog.jooq.org/feed');
+//$content = simplexml_load_file('https://css-tricks.com/feed');
+//$content = simplexml_load_file('http://feeds.hanselman.com/ScottHanselman');
+//$content = simplexml_load_file('https://www.reddit.com/r/programming/.rss');
+//$content = simplexml_load_file('http://tympanus.net/codrops/feed/');
+//$content = simplexml_load_file('https://blog.golang.org/feed.atom?format=xml');
+//$content = simplexml_load_file('https://jeremykun.com/feed');
+$content = simplexml_load_file('https://blog.codepen.io/feed');
+
+
